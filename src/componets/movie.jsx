c0d7bc6c349eb464c33cda669561798da201f63c
Oliver Galante <oliverandcarli@gmail.com>
2023-09-29 14:36:33 -0400

    Initall commit

---
+++ src/componets/movie.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+export const MovieView = () => {
+	const [movieTitle, setMovieTitle] = useState([]);
+	const [movieValue, setMovieValue] = useState({});
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		setCount(movieValue?.Search?.length);
+	}, [movieValue]);
+
+	const apiUrl = `http://www.omdbapi.com/?s=${movieTitle}&apikey=b684b3e`;
+
+	const getMovie = async () => {
+		const response = await fetch(apiUrl);
+		const data = await response.json();
+		setMovieValue(data);
+		console.log(data);
+	};
+
+	const handleSubmit = (e) => {
+		try {
+			console.log(e);
+			getMovie();
+			setMovieTitle("");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleChange = (e) => {
+		console.log(e.target.value);
+		setMovieTitle(e.target.value);
+	};
+
+	return (
+		<>
+			<form>
+				<label>
+					Search For a Movie Title
+					<input
+						type="text"
+						placeholder="Enter Movie title"
+						value={movieTitle}
+						onChange={handleChange}
+					/>
+				</label>
+				<button type="button" onClick={handleSubmit}>
+					Search Movie Title
+				</button>
+				<p>Number of movies returned: {count} </p>
+			</form>
+			{movieValue?.Search?.map((moviesTitles) => {
+				return (
+					<div key={moviesTitles.imdbID}>
+						<p>{moviesTitles.Title}</p>
+						<img src={moviesTitles.Poster}></img>
+					</div>
+				);
+			})}
+			<div></div>
+		</>
+	);
+};
